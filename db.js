ca106e680449a4bef7e53df54de6368b0c39a639
FauzanAzima26 <fauzannazimaaa123@gmail.com>
2025-11-05 23:06:36 +0700

    instalassi postgre

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // wajib untuk koneksi Supabase
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
